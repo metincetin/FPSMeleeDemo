7b491e0a24c734256d498d412fddc6da4d92f096
Metin ÇETİN <metinc309@gmail.com>
2024-10-14 15:43:52 +0300

    added decals

--- Assets/Scripts/FPSMeleeDemo/FX/AttackerFXFactory.cs
+++ Assets/Scripts/FPSMeleeDemo/FX/AttackerFXFactory.cs
@@ -1,6 +1,7 @@
 using FPSMeleeDemo.Data.Sets;
 using FPSMeleeDemo.FPS;
 using UnityEngine;
+using UnityEngine.Pool;
 
 namespace FPSMeleeDemo.FX
 {
@@ -11,16 +12,25 @@ namespace FPSMeleeDemo.FX
 		[SerializeField]
 		private MaterialGameObjectSet _particleSet;
 
+		[SerializeField]
+		private MaterialGameObjectSet _decalSet;
+
+		[SerializeField]
+		private float _releaseDuration = 4f;
+
 		public void CreateHitEffect(DamageArea.DamageHitInfo info)
 		{
 			var p = info.Point;
-
-			// var decalInst = UnityEngine.Object.Instantiate(_decal, p, Quaternion.identity);
-			// decalInst.transform.up = info.Normal;
+			var decalInst = UnityEngine.Object.Instantiate(_decalSet.GetOrDefault(info.Collider.sharedMaterial), p + info.Normal * 0.01f, Quaternion.identity);
+			decalInst.transform.forward = -info.Normal;
+			Debug.Log(Mathf.Atan2(info.RelativeVelocity.z, info.RelativeVelocity.x));
+			decalInst.transform.Rotate(info.Normal, Mathf.Atan2(info.RelativeVelocity.z, info.RelativeVelocity.x), Space.World);
+			Destroy(decalInst, _releaseDuration);
 
 			var particle = UnityEngine.Object.Instantiate(_particleSet.GetOrDefault(info.Collider.sharedMaterial), p, Quaternion.identity);
 			particle.transform.forward = -info.RelativeVelocity.normalized;
 
+			Destroy(particle, _releaseDuration);
 		}
 	}
 }
