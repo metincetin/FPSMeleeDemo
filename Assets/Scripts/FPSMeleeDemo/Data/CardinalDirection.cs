e073426e275c02ad607c8bea61f99caf9bfe3991
Metin ÇETİN <metinc309@gmail.com>
2024-10-20 04:04:11 +0300

    moved Vector2Extensions to cardinal direction class

--- Assets/Scripts/FPSMeleeDemo/Data/CardinalDirection.cs
+++ Assets/Scripts/FPSMeleeDemo/Data/CardinalDirection.cs
@@ -1,3 +1,5 @@
+using UnityEngine;
+
 namespace FPSMeleeDemo.Data
 {
     public enum CardinalDirection
@@ -10,5 +12,34 @@ namespace FPSMeleeDemo.Data
 		NortEast,
 		SouthWest,
 		SouthEast,
+	}
+    public static class CardinalDirectionExtensions
+	{
+		public static Vector2 ToVector(CardinalDirection cardinalDirection)
+		{
+			switch (cardinalDirection)
+			{
+				case CardinalDirection.West:
+					return Vector2.left;
+				case CardinalDirection.East:
+					return Vector2.right;
+				case CardinalDirection.North:
+					return Vector2.up;
+				case CardinalDirection.South:
+					return Vector2.down;
+			}
+
+			return Vector2.right;
+		}
+		
+		public static CardinalDirection ToCardinal(this Vector2 value)
+		{
+			if (Mathf.Abs(value.x) > Mathf.Abs(value.y))
+			{
+				return value.x > 0 ? CardinalDirection.East : CardinalDirection.West;
+			}
+
+			return value.y > 0 ? CardinalDirection.North: CardinalDirection.South;
+		}
 	}
 }
